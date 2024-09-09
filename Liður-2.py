99770e8542c1279c46314cb8c21c79ab43427142
AldaAsl <aau2@hi.is>
2024-09-09 12:08:47 +0000

    Bætti pattern og matches við kóðann og gerði textaskjal fyrir lið2

--- Liður-2.py
+++ Liður-2.py
@@ -2,23 +2,6 @@ import re
 import argparse
 import os
 
-#Finna segð sem les helgaingim@hi.is, hei2@hi.is, john.smith@new-world.us, python101@regex101.edu.com og john.smith@new-world 
-# en ekki @noaddress.com, plainaddress, of@langt.domainnafn, jane.doe@@hi.is, username@.com, email@domain..com
-
-# Segðin er: ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,6}(?:\.[a-zA-Z]{2,6})?$
-
-#Útskýring: 
-# [a-zA-Z0-9._%+-]+ segir að innihald svigans getur innihaldið hvaða stafi (bæði há- og lágstafi), tölur, og táknin . (punktur), _ (undirstrik), %, +, og -.
-# + þarf að vera í lokin þar sem eitt af þessum merkjum þarf að vera í emailinu allavega einu sinni 
-# @ sem skilur notendanafnið frá léninu í tölvupóstfangi.
-# [a-zA-Z0-9-]+. Þetta skilgreinir lénshlutann fyrir framan punktinn (t.d. hi í hi.is). Leyfilegt er að hafa stafi (há- og lágstafi), tölustafi og bandstrik - í þessum hluta.+ merkið gefur til kynna að það þarf að vera að minnsta kosti einn slíkur stafur.
-# \.[a-zA-Z]{2,6}: Þetta tryggir að topp-lénið (t.d. .com, .is) sé á milli 2 og 6 stafa langt. 
-# og þarf backslash að vera á undan punkti svo punkturinn þýðir ekki hvaða staf sem er heldur bókstaflega punkt. 
-# (?:\.[a-zA-Z]{2,6})? tryggir að undir-lén (t.d. .co.uk) sé einnig á milli 2 og 6 stafa langt, en er valfrjálst.
-# Spurningarmerkið ? á eftir hópnum gerir allt þetta valfrjálst (það má koma einu sinni eða ekki koma).
-
-
-
 def parse_arguments():
     """
     Lesa inn argument frá skipanalínu
@@ -57,12 +40,14 @@ def finna_netfong (text):
     compiled_pattern = re.compile(pattern)
     matches = compiled_pattern.finditer(text)
     
+    return matches
+    
     # Búa til lista fyrir niðurstöður
     results = []
 
     raise NotImplementedError("Regluleg segð til að finna netföng hefur ekki verið útfærð.")
 
-    
+
 
 def prenta_nidurstodur(netfong_listi):
     """
